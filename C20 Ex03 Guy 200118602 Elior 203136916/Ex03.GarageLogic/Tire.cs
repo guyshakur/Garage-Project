78e9fd51f9fb1c07c9751e58c39c223b015df4d4
guy schiff <guyshakur@gmail.com>
2020-09-05 20:09:19 +0300

    created and implemented tire and vehicle classes

---
+++ C20 Ex03 Guy 200118602 Elior 203136916/Ex03.GarageLogic/Tire.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public class Tire
+    {
+        private string m_Brand;
+
+        public string Brand
+        {
+            get
+            {
+                return m_Brand;
+            }
+            set
+            {
+                m_Brand = value;
+            }
+        }
+
+        private float m_CurrentPeerPressure;
+
+        public float CurrentPeerPressure
+        {
+            get
+            {
+                return m_CurrentPeerPressure;
+            }
+            set
+            {
+                m_CurrentPeerPressure = value;
+            }
+
+        }
+
+        private float m_MaxPeerPressure;
+
+        public float MaxPeerPressure
+        {
+            get
+            {
+                return m_MaxPeerPressure;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Only positive value are allowed");
+                }
+                else
+                {
+                    m_MaxPeerPressure = value;
+                }
+            }
+        }
+
+        public Tire(string i_Brand,float i_CurrentPeerPressure,float i_MaxPeerPressue)
+        {
+            Brand= i_Brand;
+            CurrentPeerPressure = i_CurrentPeerPressure;
+            MaxPeerPressure = i_MaxPeerPressue;
+
+
+        }
+
+        public void InflateTire(float i_AmountOfAirToAdd)
+        {
+            if (i_AmountOfAirToAdd + CurrentPeerPressure > MaxPeerPressure)
+            {
+                throw new ArgumentException("The max peer pressure for this tire is " + MaxPeerPressure);
+            }
+            else
+            {
+                CurrentPeerPressure += i_AmountOfAirToAdd;
+            }
+
+        }
+
+    }
+}
---
+++ C20 Ex03 Guy 200118602 Elior 203136916/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+        private string m_Model;
+
+        public string Model
+        {
+            set
+            {
+                m_Model = value ;
+            }
+            get
+            {
+                return m_Model;
+            }
+
+        }
+
+        private string m_LicenceID;
+
+        public string LicenceID
+        {
+            set
+            {
+                m_LicenceID = value;
+            }
+            get
+            {
+                return m_LicenceID;
+            }
+        }
+
+        private float m_EnergyPercent;
+
+        public float EnergyPercent
+        {
+            get
+            {
+                return m_EnergyPercent;
+            }
+
+            set
+            {
+                m_EnergyPercent = value;
+            }
+        }
+
+        private List<Tire> m_wheels;
+
+        public List<Tire> Tires
+        {
+            get
+            {
+                return m_wheels;
+            }
+            set
+            {
+                m_wheels = value;
+            }
+        }
+    }
+}
